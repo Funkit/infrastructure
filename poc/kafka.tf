c4c3268a8552777e33c0a8b46fb677d550630713
Camille Chomel <camille.chomel@gmail.com>
2023-05-14 13:23:44 +0200

    working

--- poc/kafka.tf
+++ poc/kafka.tf
@@ -1,19 +1,17 @@
-data "linode_profile" "me" {}
-
 data "linode_sshkey" "my_key" {
-  label = "personal computer"
+  label = "personal"
 }
 
 resource "linode_instance" "kafka-test" {
   label = "kafka-test"
-  type  = "g6-nanode-1"
+  type  = "g6-standard-2"
   image = "linode/ubuntu18.04"
   region = "eu-central"
+  tags = [ "personal" ]
 
   backups_enabled = false
 
-  authorized_users = [ data.linode_profile.me.username ]
-  authorized_keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6KVjvz1XdVomwzuqWnljsY6FRw2Y09NbbACXxpp9RcpGA8WERhfB4UPWBzy0ECX9vcR+hIMdqilGYhQgShwnSRis9K/NNgVKWZIchp1PfSHkpATRc3cBwOMGBaK8DiEAioUrwPXMnVs6CDGOhDEiE3P6FGXYhEiaAUI+eSZb9Y14crwxu9JeGc/ATyAgKw6pUPKKZKJMncaY8NNuP5kOacrrmpmi6bZe1t9fCBu3AIwrCEogokngHGiYIHZ1z631Vo1nmMwWqqQihTPfLl9bCWMZfpuwfLmGbowYDLAK2tp6W0qXOSLOKSqbhWJMDgzrCiZ28jkaFfw9vM/8p6J3HSiTd7Qoj59txJzhTb962PCSI2KRgUfTis3Mv2T491AKrT2j3/ygK4Jzh9NoTXhle2FgOnOlx3PmKMu58iLERFE3WZmJApBDsyhlcPj9lRp+kA/Q3M0mmHSOvvzYfLi2xQSJeGJkfMgxfpCMG+2SJylWSRw/xPOzcV4VfTcFuZoM= camille chomel@funkit" ]
+  authorized_keys = [ data.linode_sshkey.my_key.ssh_key ]
 }
 
 data "cloudflare_zone" "my_zone" {
